f5973d2462bea9e6aada479dbc09261f70721453
varage <lucashanauer21@gmail.com>
2025-09-03 21:51:28 -0300

    concatenacao

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,18 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500.00;
+        System.out.println("O valor do salário mínimo é: " + salarioMinimo);
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+        System.out.println("O valor de numeroCurto2 é: " + numeroCurto2);
 
-        int numero = 5;
-        numero = 10;
+        int numero = 10;
         System.out.println("O valor de numero é: " + numero);
 
         final double VALOR_DE_PI = 3.14;
-        System.out.println("O valor de PI é: " + VALOR_DE_PI);
-
-        
+        System.out.println("O valor de PI é: " + VALOR_DE_PI);  
     }
 }
