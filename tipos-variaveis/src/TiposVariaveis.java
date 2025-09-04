247a1cd64feb169e264c14fb3a61733224b99b99
varage <lucashanauer21@gmail.com>
2025-09-03 21:15:00 -0300

    Tipos e variaveis estudadas

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -5,5 +5,14 @@ public class TiposVariaveis {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println("O valor de numero é: " + numero);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println("O valor de PI é: " + VALOR_DE_PI);
+
+        
     }
 }
